05667ee30bfcd428824136e68a6cd87f986a77c6
Rodrigo Monteiro <rodrigomonteiiro17@gmail.com>
2023-05-05 11:10:47 -0300

    Frontend - Start implementation Home view

--- frontend/quiztech/src/app/app.component.ts
+++ frontend/quiztech/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  showSplashScreenContent: boolean = true;
   title = 'quiztech';
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+
+
+
+  async handleRedirectHomePage() {
+    await this.router.navigate(['home']);
+    // console.log(this.router.url);
+    // console.log(this.router.url.length > 1);
+    this.handleToggleShowRouterContent();
+  }
+
+  handleToggleShowRouterContent() {
+    const hasParams = this.router.url.length > 1;
+    console.log(hasParams)
+    this.showSplashScreenContent = !hasParams;
+  }
 }
