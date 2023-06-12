262dc1766754487fd89b263cbb37938565743be9
Rodrigo Monteiro <rodrigomonteiiro17@gmail.com>
2023-06-12 14:56:43 -0300

    Backend: Create update &  getById methods on questionController

--- backend/demo/src/main/java/com/quiz/demo/controller/QuestionController.java
+++ backend/demo/src/main/java/com/quiz/demo/controller/QuestionController.java
@@ -1,11 +1,16 @@
 package com.quiz.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.quiz.demo.repository.QuestionRepository;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 
 import com.quiz.demo.model.Question;
 
@@ -31,9 +37,53 @@ public class QuestionController {
     public List<Question> list() {
         return questionRepository.findAll();
     }
-     @PostMapping // @ReqestMapping(method = RequestMethod.GET)
+
+    @GetMapping("/{id}")
+    public Optional<Question> getQuestionById(@PathVariable @Positive Long id) {
+        return questionRepository.findById(id);
+
+    }
+
+    @PostMapping 
     @ResponseStatus(code = HttpStatus.CREATED)
     public Question create(@RequestBody @Valid Question question) {
         return questionRepository.save(question);
     }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable @Positive Long id) {
+        Optional<Question> questionOptional = questionRepository.findById(id);
+
+        if (questionOptional.isPresent()) {
+            Question question = questionOptional.get();
+            questionRepository.delete(question);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Question> update(@PathVariable Long id, @RequestBody @Valid Question updatedQuestion) {
+        Optional<Question> optionalQuestion = questionRepository.findById(id);
+
+        if (optionalQuestion.isPresent()) {
+
+            Question existingQuestion = optionalQuestion.get();
+
+            existingQuestion.setText(updatedQuestion.getText());
+            existingQuestion.setArea(updatedQuestion.getArea());
+            existingQuestion.setSubject(updatedQuestion.getSubject());
+            existingQuestion.setDifficult(updatedQuestion.getDifficult());
+            existingQuestion.setAnswear(updatedQuestion.getAnswear());
+            existingQuestion.setAlternatives(updatedQuestion.getAlternatives());
+
+            questionRepository.save(existingQuestion);
+
+            return ResponseEntity.ok(existingQuestion);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
