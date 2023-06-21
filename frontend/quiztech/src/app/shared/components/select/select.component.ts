d585eb73f162dc185d6eceae6d7d96a9e2e91ef7
Rodrigo Monteiro <rodrigomonteiiro17@gmail.com>
2023-06-21 13:00:54 -0300

    Frontend: Changing select component

--- frontend/quiztech/src/app/shared/components/select/select.component.ts
+++ frontend/quiztech/src/app/shared/components/select/select.component.ts
@@ -9,21 +9,40 @@ import { ThemeService } from 'src/app/styles/theme/theme.service';
 })
 export class SelectComponent implements OnInit {
   listThemeItems: Theme[] = [];
+  isSelectOpen: boolean = false;
 
-  constructor(private themeService: ThemeService) {
-
-  }
+  constructor(private themeService: ThemeService) {}
 
   ngOnInit(): void {
     this.listThemeItems = allThemes;
   }
 
   switchTheme(techName: string, theme: string) {
+    this.handleCloseSelect()
     this.listThemeItems.forEach((e) =>
       e.techName === techName && e.theme === theme
         ? this.themeService.setTheme(e)
         : ''
     );
-    console.log('New theme is ', this.themeService.currentTheme);
+
+  }
+  handleOpeneSelect() {
+    this.isSelectOpen = true;
+  }
+
+  handleCloseSelect() {
+    this.isSelectOpen = false;
+  }
+
+  handleToggleOpenSelect() {
+    this.isSelectOpen = !this.isSelectOpen;
+  }
+
+  getCurrentThemeName(): string{
+    return this.themeService.getCurrentTheme().techName
+  }
+
+  getCurrentThemeMode(): string{
+    return this.themeService.getCurrentTheme().theme
   }
 }
